fe3dcb5817b38b57a6abd87da94946e6afa3b22f
liaozhimingandy@qq.com <liaozhimingandy@qq.com>
2024-11-26 16:45:45 +0800

    chore: 初始化仓库及常用组件依赖

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import zhCN from "ant-design-vue/lib/locale/zh_CN"; // 导入中文语言包
+
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+dayjs.locale("zh-cn");
+
+</script>
+
+<template>
+  <a-config-provider :locale="locale">
+    <router-view/>
+  </a-config-provider>
+</template>
+
+<style scoped>
+/* 自定义进度条颜色 */
+#nprogress .bar{
+  background: #027AFF !important;
+}
+/* 自定义进度条位置 */
+#nprogress .bar {
+  height: 5px; /* 调整进度条的高度 */
+  top: 0; /* 使进度条紧贴顶部 */
+  position: fixed;
+  left: 0;
+  width: 100%;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import {version} from './package.json'
+import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import removeConsole from 'vite-plugin-remove-console'
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: '/openaim/', // 这里保持和你你的 GitHub 仓库名一致
+    define: {
+        __APP_VERSION__: JSON.stringify(version),
+    },
+    server: {
+        host: process.env.VITE_HOST || '0.0.0.0',
+    },
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [
+                AntDesignVueResolver()
+            ],
+        }),
+        Components({
+            resolvers: [
+                AntDesignVueResolver({
+                    importStyle: false, // css in js
+                }),
+            ],
+        }),
+        removeConsole({
+            // 默认在生产环境移除console
+            exclude: ['error', 'warn'], // 可选：保留 error 和 warn 信息
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // 这里设置@为src目录别名
+        },
+    },
+})
